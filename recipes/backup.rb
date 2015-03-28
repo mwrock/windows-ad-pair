c6ba0d89f731ac71d7d74deda2648e93721ba97d
mwrock <matt@mattwrock.com>
2015-03-28 03:11:33 -0700

    in the event of multiple nics add the primary ip to the nic in the same subnet

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -1,14 +1,17 @@
 primary = search_for_nodes("run_list:*windows_ad_pair??primary*")
 primary_ip = primary[0]['ipaddress']
+primary_subnet = primary_ip.split('.')[0..2].join('.')
 
 if primary_ip != "127.0.0.1"
   powershell_script 'setting primary dns server to primary ad' do
     code <<-EOS
-      Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses '#{primary_ip}'
+      Get-NetIPConfiguration | ? { 
+        $_.IPv4Address.IPAddress.StartsWith("#{primary_subnet}") 
+        } | Set-DnsClientServerAddress -ServerAddresses '#{primary_ip}'
     EOS
 
     guard_interpreter :powershell_script
-    not_if "(Get-NetAdapter | Get-DnsClientServerAddress -AddressFamily IPv4).ServerAddresseses -contains '#{primary_ip}'"
+    not_if "(Get-DnsClientServerAddress -AddressFamily IPv4 | ? { $_.ServerAddresseses -contains '#{primary_ip}' }).count -gt 0"
   end
 end
 
